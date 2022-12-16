5cedd3c890a7acc99a6c18564c343ff06a889650
SagaSherwin <sherwinnikolaisaga@gmail.com>
2022-12-16 18:24:55 +0800

    Rename SimpleCalc Package

---
+++ src/SimpleCalc/Calc.java
@@ -0,0 +1,16 @@
+package SimpleCalc;
+
+import javax.swing.*;
+
+public class Calc {
+    private JPanel panel1;
+    private JTextField tfNumber1;
+    private JComboBox cbOperations;
+    private JButton btnCompute;
+    private JTextField tfNumber2;
+    private JTextField lblResult;
+
+
+
+
+}
