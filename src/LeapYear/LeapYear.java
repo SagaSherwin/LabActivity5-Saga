5a36ce53e4427804f4dd2b48da3e463dae1009f9
SagaSherwin <sherwinnikolaisaga@gmail.com>
2022-12-16 17:52:45 +0800

    progress

--- src/LeapYear/LeapYear.java
+++ src/LeapYear/LeapYear.java
@@ -1,14 +1,30 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LeapYear extends JFrame{
 
     private JPanel panel1;
-    private JTextField textField1;
-    private JButton checkButton;
+    private JTextField tfYear;
+    private JButton btnCheckYear;
+
+    public LeapYear(){
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double year = Double.parseDouble(tfYear.getText());
+
+            }
+        });
+    }
 
     public static void main(String[] args) {
-
+        LeapYear year = new LeapYear();
+        year.setContentPane(year.panel1);
+        year.setSize(400, 600);
+        year.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        year.setVisible(true);
     }
 }
