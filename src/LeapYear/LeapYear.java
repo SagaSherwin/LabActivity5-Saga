ca8cd9fcc19973e27b0cb399939b3b06432c294a
SagaSherwin <sherwinnikolaisaga@gmail.com>
2022-12-16 17:32:50 +0800

    Add leapYear main function

--- src/LeapYear/LeapYear.java
+++ src/LeapYear/LeapYear.java
@@ -2,8 +2,13 @@ package LeapYear;
 
 import javax.swing.*;
 
-public class LeapYear {
+public class LeapYear extends JFrame{
+
     private JPanel panel1;
     private JTextField textField1;
     private JButton checkButton;
+
+    public static void main(String[] args) {
+
+    }
 }
