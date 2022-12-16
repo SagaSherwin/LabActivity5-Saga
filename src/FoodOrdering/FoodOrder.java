24808f2ef16aa04cfdbe0deeee3edd3ce0c725bc
SagaSherwin <sherwinnikolaisaga@gmail.com>
2022-12-16 19:39:39 +0800

    Add FoodOrder main function

--- src/FoodOrdering/FoodOrder.java
+++ src/FoodOrdering/FoodOrder.java
@@ -2,9 +2,13 @@ package FoodOrdering;
 
 import javax.swing.*;
 
-public class FoodOrderGUI {
+public class FoodOrder extends JFrame{
     private JPanel panel1;
     private JCheckBox pizzaCheckBox;
     private JRadioButton noneRadioButton;
     private JButton orderButton;
+
+    public static void main(String[] args) {
+
+    }
 }
